ca4d663b08ad191a1ebd97176f8b20a48354d4a0
NewLezaiYa <3651440375@qq.com>
2025-10-03 11:16:10 +0800

    修复变形失效的bug

--- Modules/CustomRolesHelper.cs
+++ Modules/CustomRolesHelper.cs
@@ -519,7 +519,7 @@ public static class CustomRolesHelper
         {
             CustomRoles.Charmed => Cultist.CanBeCharmed(target),
             CustomRoles.Madmate => recruiter.Is(CustomRoles.Gangster) ? target.CanBeMadmate(forGangster: true) : target.CanBeMadmate(forAdmirer: true),
-            CustomRoles.Admired => Admirer.CanBeAdmired(target, recruiter),
+            CustomRoles.Admired => Admirer.CanBeAdmired(target),
             CustomRoles.Enchanted => Ritualist.CanBeConverted(target),
             CustomRoles.Recruit => Jackal.CanBeSidekick(target),
             CustomRoles.Infected => Infectious.CanBeBitten(target),
